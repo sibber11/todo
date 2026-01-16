e870842b6a1f457dbcb539e48354993aa535a7ae
Sibber Ahmad <sibber11@gmail.com>
2026-01-16 22:02:09 +0600

    url update

--- frontend/src/stores/auth.js
+++ frontend/src/stores/auth.js
@@ -2,7 +2,7 @@ import { defineStore } from "pinia";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/backend/api",
+  baseURL: "/api",
 });
 
 export const useAuthStore = defineStore("auth", {
